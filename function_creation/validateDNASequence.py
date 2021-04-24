f0eb24afaa495b01a2c32e700ac339834af8e729
Rishabhraghwendra18 <rishabhraghwendra2002@gmail.com>
2021-04-24 21:39:52 +0530

    Added function to validate DNA Sequence

---
+++ function_creation/validateDNASequence.py
@@ -0,0 +1,17 @@
+
+typesOfNucleotides =["A","C","G","T"]
+
+def validateDNASequence(DNASequence):
+      '''
+            This function verifies whether the passed in sequence 
+            is an DNA sequence or not . Returns False if not ,
+            otherwise returns the original sequence .
+      '''
+      sequence=DNASequence.upper()
+      for nuc in sequence:
+            if nuc not in typesOfNucleotides:
+                  return False
+      return DNASequence
+
+
+print(validateDNASequence("AttCgggGt"))
